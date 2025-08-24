de23a7114f575631fb1db58b292e01ba82238e79
WorldFS <fsworld009@gmail.com>
2025-08-24 19:24:52 +0800

    prepare_dev_env: fix patch command

--- utils/prepare_dev_env.sh
+++ utils/prepare_dev_env.sh
@@ -3,8 +3,7 @@ set -e
 
 # Create necessary files for local development
 
-
-version="092"
+pushd "$(dirname "$0")" > /dev/null
 version=${1:-"092"}
 
 # Create data
@@ -13,6 +12,6 @@ node create_id_tables.js ../RouteMatriXRandomizer/$version/data ../src/$version/
 # Create and patch boot.lua in modded_script
 cd ..
 cp RouteMatriXRandomizer/$version/data/multiworld/boot.lua src/$version/
-patch -p0 < src/$version/boot_lua.patch
+patch -u src/$version/boot.lua src/$version/boot_lua.patch
 
 popd > /dev/null
